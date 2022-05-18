f11f4b00c8b1f87367d6ddbc6b3a14267b2b5db9
Hanan-Hussein <hanhuss035@gmail.com>
2022-05-18 13:34:33 +0300

    ft:navbar-configured dynamic navbar based on authenticated user

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from flask import render_template, url_for, flash, redirect, request
-from app.users.forms import Register, Login,Subscribe
+from app.users.forms import Register, Login,Subscribe,UpdateAccountForm
 from app import db,bcrypt,mail
 from flask_mail import  Message
-from app.models import User, Subscribers
+from app.models import Posts, User, Subscribers
+from app.users.utils import save_picture
 
 users = Blueprint('users', __name__)
 
@@ -58,7 +59,7 @@ def subscribe():
         
         db.session.add(sub)
         db.session.commit()
-        msg=Message("WELCOME",sender="apollolibrary99@gmail.com",recipients=[ussuber.email])
+        msg=Message("WELCOME",sender="apollolibrary99@gmail.com",recipients=[sub.email])
         msg.body = "Welcome to H&J's Blog you Have Subscribed News Letter we will loop you on all newly updates"
         mail.send(msg)
      
@@ -67,3 +68,29 @@ def subscribe():
 
 
     return render_template('subscribe.html',form=form)
+
+@users.route('/account',methods=['POST', 'GET'])
+@login_required
+def account():
+    """
+    """
+    post= Posts.query.filter_by(user_id=current_user.id)
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture=save_picture(form.picture.data)
+            current_user.profile=picture
+             
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+
+        db.session.commit()
+        flash('Your Account Has been updated!','success')
+        return redirect(url_for('users.account'))
+    elif request.method == 'GET':
+
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    
+    image_file= url_for('static',filename='profiles/'+current_user.image_file)
+    return render_template('account.html', title='Account',image_file=image_file,form=form,post=post )
